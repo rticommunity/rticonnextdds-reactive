dd70f90ca9e8fcb3b84bb9dc94635efb889af0ae
Sumant Tambe <sumant@mint.rti.com>
2015-08-17 16:03:41 -0700

    working from_keyed_topic for RxCpp

--- cpp/rx4dds/include/rx4dds/rx4dds.h
+++ cpp/rx4dds/include/rx4dds/rx4dds.h
@@ -2,11 +2,43 @@
 
 #include <dds/sub/ddssub.hpp>
 #include <dds/core/ddscore.hpp>
+
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <unordered_map>
+#include <functional>
 
 #include "rxcpp/rx.hpp"
+
+namespace std
+{
+  template <>
+  struct hash<dds::core::InstanceHandle>
+  {
+    std::size_t operator()(const dds::core::InstanceHandle& ihandle) const
+    {
+      std::string handle;
+      handle.resize(64);
+      std::ostringstream out(handle);
+      out << ihandle;
+      return std::hash<string>()(handle);
+    }
+
+  };
+
+} // namespace std
+
+namespace dds {
+  namespace core {
+
+    bool operator < (const dds::core::string & lhs, const dds::core::string & rhs)
+    {
+      return strcmp(lhs.c_str(), rhs.c_str()) < 0;
+    }
+
+  } // namespace core
+} // namespace dds
 
 namespace rx4dds {
 
@@ -82,7 +114,8 @@ namespace rx4dds {
             dds::sub::Subscriber(state_->participant_),
             state_->topic_);
 
-          auto subscriber = subject.get_subscriber();
+          typename rxcpp::subjects::subject<T>::subscriber_type subscriber = 
+            subject.get_subscriber();
 
           auto schedule_dr_subscription_end = [this]()
           {
@@ -99,41 +132,45 @@ namespace rx4dds {
             state_->reader_,
             dds::sub::status::DataState::any(),
             [this, subscriber, schedule_dr_subscription_end]()
-          {
-            try {
-              dds::sub::LoanedSamples<ShapeTypeExtended> samples =
-                state_->reader_.take();
+            {
+              try {
+                dds::sub::LoanedSamples<ShapeTypeExtended> samples =
+                  state_->reader_.take();
 
-              for (auto sample : samples)
-              {
-                dds::sub::status::InstanceState istate;
-                sample.info().state() >> istate;
+                for (auto sample : samples)
+                {
+                  dds::sub::status::InstanceState istate;
+                  sample.info().state() >> istate;
 
-                if (sample.info().valid()) {
-                  subscriber.on_next(sample.data());
-                }
-                else if (istate == dds::sub::status::InstanceState::not_alive_no_writers())
-                {
-                  subscriber.on_error(
-                    std::make_exception_ptr(
-                    NotAliveNoWriters("No alive writers for topic = " + state_->topic_name_)));
-                  schedule_dr_subscription_end();
-                  break;
-                }
-                else if (istate == dds::sub::status::InstanceState::not_alive_disposed())
-                {
-                  subscriber.on_completed();
-                  schedule_dr_subscription_end();
-                  break;
+                  if (sample.info().valid())
+                  {
+                    // RxCpp documentation suggests that the following check is 
+                    // is a good practice. See docs for rxcpp::observable<void,void>
+                    if(subscriber.is_subscribed())
+                      subscriber.on_next(sample.data());
+                  }
+                  else if (istate == dds::sub::status::InstanceState::not_alive_no_writers())
+                  {
+                    subscriber.on_error(
+                      std::make_exception_ptr(
+                      NotAliveNoWriters("No alive writers for topic = " + state_->topic_name_)));
+                    schedule_dr_subscription_end();
+                    break;
+                  }
+                  else if (istate == dds::sub::status::InstanceState::not_alive_disposed())
+                  {
+                    subscriber.on_completed();
+                    schedule_dr_subscription_end();
+                    break;
+                  }
                 }
               }
-            }
-            catch (...)
-            {
-              subscriber.on_error(std::current_exception());
-              schedule_dr_subscription_end();
-            }
-          });
+              catch (...)
+              {
+                subscriber.on_error(std::current_exception());                
+                schedule_dr_subscription_end();
+              }
+            });
 
           state_->wait_set_ += state_->read_condition_;
           state_->initdone_ = true;
@@ -142,10 +179,50 @@ namespace rx4dds {
 
     };
 
-    template <class T>
+    template <class Key, class T, class KeySelector>
     class KeyedSubscription
     {
     private:
+      typedef rxcpp::grouped_observable<Key, T> GroupedObservable;
+
+      class Bucket 
+      {
+        rxcpp::subjects::subject<T> subject_;
+        rxcpp::composite_subscription subscription_;
+        Key key_;
+
+      public:
+
+        Bucket() {}
+
+        Bucket(KeySelector key_selector, 
+               rxcpp::subjects::subject<GroupedObservable> topsubject)
+        { 
+          subscription_ =
+            subject_
+            .get_observable()
+            .group_by(std::move(key_selector),
+                      [](T & t) { return t; })
+            .map([this, topsubject](GroupedObservable go) {
+                   const_cast<Key &>(key_) = go.get_key();
+                   const_cast<rxcpp::subjects::subject<GroupedObservable> &>(topsubject).get_subscriber().on_next(go);
+                    return 0;
+          }).subscribe();
+        }
+
+        rxcpp::subjects::subject<T> & get_subject()
+        {
+          return subject_;
+        }
+        
+        const Key & get_key() const
+        {
+          return key_;
+        }
+      };
+
+      typedef std::unordered_map<dds::core::InstanceHandle, Bucket> BucketMap;
+
       struct SubscriptionState
       {
         bool initdone_;
@@ -156,11 +233,14 @@ namespace rx4dds {
         dds::core::cond::WaitSet wait_set_;
         dds::sub::cond::ReadCondition read_condition_;
         rxcpp::schedulers::worker worker_;
+        BucketMap buckets_;
+        KeySelector key_selector_;
 
         SubscriptionState(dds::domain::DomainParticipant part,
                           const std::string & topic_name,
                           dds::core::cond::WaitSet wait_set,
-                          rxcpp::schedulers::worker worker)
+                          rxcpp::schedulers::worker worker,
+                          KeySelector&& key_selector)
           : initdone_(false),
             participant_(part),
             topic_name_(topic_name),
@@ -168,7 +248,8 @@ namespace rx4dds {
             reader_(dds::core::null),
             wait_set_(wait_set),
             read_condition_(dds::core::null),
-            worker_(worker)
+            worker_(worker),
+            key_selector_(std::move(key_selector))
         { }
 
         ~SubscriptionState()
@@ -184,12 +265,13 @@ namespace rx4dds {
       KeyedSubscription(dds::domain::DomainParticipant part,
                         const std::string & topic_name,
                         dds::core::cond::WaitSet wait_set,
-                        rxcpp::schedulers::worker worker)
+                        rxcpp::schedulers::worker worker,
+                        KeySelector key_selector)
         : state_(std::make_shared<SubscriptionState>(
-                    part, topic_name, wait_set, worker))
+                    part, topic_name, wait_set, worker, std::move(key_selector)))
       { }
 
-      void initialize_once(rxcpp::subjects::subject<T> subject)
+      void initialize_once(rxcpp::subjects::subject<GroupedObservable> topsubject)
       {
         if (!state_->initdone_)
         {
@@ -200,15 +282,13 @@ namespace rx4dds {
             dds::sub::Subscriber(state_->participant_),
             state_->topic_);
 
-          auto subscriber = subject.get_subscriber();
-
           auto schedule_dr_subscription_end = [this]()
           {
             state_->worker_.schedule(
               rxcpp::schedulers::make_schedulable(
               state_->worker_,
               [this](const rxcpp::schedulers::schedulable &) {
-              state_.reset();
+                state_.reset();
             }));
           };
 
@@ -216,7 +296,7 @@ namespace rx4dds {
             dds::sub::cond::ReadCondition(
             state_->reader_,
             dds::sub::status::DataState::any(),
-            [this, subscriber, schedule_dr_subscription_end]()
+            [this, topsubject, schedule_dr_subscription_end]() mutable
           {
             try {
               dds::sub::LoanedSamples<ShapeTypeExtended> samples =
@@ -224,31 +304,67 @@ namespace rx4dds {
 
               for (auto sample : samples)
               {
-                dds::sub::status::InstanceState istate;
+                dds::sub::status::InstanceState istate; 
                 sample.info().state() >> istate;
+                dds::core::InstanceHandle handle = sample.info().instance_handle();
+                BucketMap::const_iterator got = state_->buckets_.find(handle);
 
-                if (sample.info().valid()) {
-                  subscriber.on_next(sample.data());
-                }
-                else if (istate == dds::sub::status::InstanceState::not_alive_no_writers())
+                if (sample.info().valid()) 
                 {
-                  subscriber.on_error(
-                    std::make_exception_ptr(
-                    NotAliveNoWriters("No alive writers for topic = " + state_->topic_name_)));
-                  schedule_dr_subscription_end();
-                  break;
+                  if (got != state_->buckets_.end()) // instance exists
+                  {
+                    state_->buckets_[handle].get_subject()
+                                            .get_subscriber()
+                                            .on_next(sample.data());
+                  }
+                  else // new instance 
+                  {
+                    state_->buckets_.emplace(
+                      std::make_pair(handle, Bucket(state_->key_selector_, topsubject)));
+
+                    // A new grouped_observable is pushed through 
+                    // topsubject before sample.data.
+                    state_->buckets_[handle].get_subject().get_subscriber().on_next(sample.data());
+                  }
                 }
                 else if (istate == dds::sub::status::InstanceState::not_alive_disposed())
                 {
-                  subscriber.on_completed();
-                  schedule_dr_subscription_end();
-                  break;
+                  if (got != state_->buckets_.end()) // instance exists
+                  {
+                    state_->buckets_[handle].get_subject()
+                                            .get_subscriber()
+                                            .on_completed();
+                    
+                    state_->buckets_.erase(handle);
+                  }
+                  else
+                  {
+                    throw std::runtime_error("Instance should really exist at this point");
+                  }
                 }
+                else if (istate == dds::sub::status::InstanceState::not_alive_no_writers())
+                {
+                  if (got != state_->buckets_.end()) // instance exists
+                  {
+                    state_->buckets_[handle]
+                          .get_subject()
+                          .get_subscriber()
+                          .on_error(std::make_exception_ptr(
+                              NotAliveNoWriters("No alive writers for instance with key = ")));
+
+                    state_->buckets_.erase(handle);
+                  }
+                  else
+                  {
+                    throw std::runtime_error("Instance should really exist at this point");
+                  }                    
+                }
+                
               }
             }
             catch (...)
             {
-              subscriber.on_error(std::current_exception());
+              topsubject.get_subscriber().on_error(std::current_exception());
               schedule_dr_subscription_end();
             }
           });
@@ -276,35 +392,52 @@ namespace rx4dds {
 
     return rxcpp::observable<>::create<T>(
       [dr_subscription, subject]
-    (rxcpp::subscriber<T> subscriber)
-    {
-      const_cast<detail::KeyLessSubscription<T> &>(dr_subscription).initialize_once(subject);
-      rxcpp::composite_subscription subscription =
-        subject.get_observable().subscribe(subscriber);
-      return subscription;
-    });
+      (rxcpp::subscriber<T> subscriber)
+      {
+        const_cast<detail::KeyLessSubscription<T> &>(dr_subscription).initialize_once(subject);
+        rxcpp::composite_subscription subscription =
+          subject.get_observable().subscribe(subscriber);
+        return subscription;
+      });
   }
 
-  template <class T>
-  rxcpp::observable<T> from_keyed_topic(
+  template <class Key, class T, class KeySelector>
+  rxcpp::observable<rxcpp::grouped_observable<Key, T>> from_keyed_topic(
     dds::domain::DomainParticipant participant,
     const std::string & topic_name,
     dds::core::cond::WaitSet wait_set,
-    rxcpp::schedulers::worker worker)
+    rxcpp::schedulers::worker worker,
+    KeySelector&& key_selector /* Key(T) */)
   {
-    detail::KeyedSubscription<T> dr_subscription(
-      participant, topic_name, wait_set, worker);
+    detail::KeyedSubscription<Key, T, KeySelector> dr_subscription(
+      participant, topic_name, wait_set, worker, std::forward<KeySelector>(key_selector));
 
-    rx::subjects::subject<T> subject;
+    rxcpp::subjects::subject<rxcpp::grouped_observable<Key, T>> subject;
 
-    return rxcpp::observable<>::create<T>(
+    return rxcpp::observable<>::create<
+      rxcpp::grouped_observable<Key, T>>(
       [dr_subscription, subject]
-    (rxcpp::subscriber<T> subscriber)
-    {
-      const_cast<detail::KeyedSubscription<T> &>(dr_subscription).initialize_once(subject);
-      rxcpp::composite_subscription subscription =
-        subject.get_observable().subscribe(subscriber);
-      return subscription;
-    });
+      (rxcpp::subscriber<rxcpp::grouped_observable<Key, T>> subscriber)
+      {
+        const_cast<detail::KeyedSubscription<Key, T, KeySelector> &>(dr_subscription).initialize_once(subject);
+        rxcpp::composite_subscription subscription =
+          subject.get_observable().subscribe(subscriber);
+        return subscription;
+      });
   }
 } // namespace rx4dds
+
+
+/*class KeyStorage
+{
+Key key_;
+public:
+
+explicit KeyStorage(const Key & k) : key_(k) { }
+explicit KeyStorage(Key&& k) : key_(std::move(k)) { }
+
+const Key & on_get_key() const
+{
+return key_;
+}
+};*/
